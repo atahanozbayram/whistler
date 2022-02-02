37b82e749d5323dc1db177307dc4c2484f7141e3
Atahan <atahan_ozbayram@hotmail.com>
2022-02-02 19:36:52 +0300

    Add routes api to application

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
+require("module-alias/register");
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 const port = process.env.PORT | 3000;
+const { userRoute } = require("@root/src/routes/user/user.route");
 
-app.get("/", (req, res) => {
-	res.send("testing");
-});
+const apiRoute = express.Router();
+apiRoute.use("/user", userRoute);
 
+app.use(bodyParser.json());
+
+app.use("/api", apiRoute);
 app.listen(port, () => {
 	console.log(`example app listening on port ${port}`);
 });
