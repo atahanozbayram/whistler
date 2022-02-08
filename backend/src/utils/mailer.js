ce7ed49244b2a39ceac158815f684ac79dba4723
Atahan <atahan_ozbayram@hotmail.com>
2022-02-08 16:35:30 +0300

    Add nodemailer transporter config

---
+++ backend/src/utils/mailer.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const process = require("process");
+
+const transporter = nodemailer.createTransport({
+	host: process.env["MAIL_HOST"],
+	port: process.env["MAIL_PORT"],
+	secure: false,
+	auth: {
+		user: process.env["MAIL_USER"],
+		pass: process.env["MAIL_PASS"],
+	},
+});
+
+module.exports = { transporter };
