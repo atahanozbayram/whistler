fe094374ea8c16a5648d4ce4d9776338e905e8e5
Atahan <atahan_ozbayram@hotmail.com>
2022-02-18 18:49:40 +0300

    Change index file to export app into new file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,9 @@
 require("module-alias/register");
 require("dotenv").config();
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+const { app } = require("@root/src/app");
 
 const port = process.env.PORT | 3000;
-const { userRoute } = require("@root/src/routes/user/user.route");
 
-const apiRoute = express.Router();
-apiRoute.use("/user", userRoute);
-
-app.use(bodyParser.json());
-
-app.use("/api", apiRoute);
 app.listen(port, () => {
 	console.log(`example app listening on port ${port}`);
 });
