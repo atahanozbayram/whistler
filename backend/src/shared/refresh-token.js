1f59b4b9ee3e207c001a3ae0e61f1db75a38c0e2
Atahan <atahan_ozbayram@hotmail.com>
2022-03-17 10:55:43 +0300

    Fix bugs that makes function completely useless

--- backend/src/shared/refresh-token.js
+++ backend/src/shared/refresh-token.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const { connection } = require("@shared/database-connection");
+const mysql = require("mysql");
+const { uuidToBinary } = require("@shared/user");
 
 const generateRefreshToken = function (expiresIn = "90 days") {
 	const token_value = crypto.randomBytes(64).toString("hex");
@@ -13,13 +15,14 @@ const generateRefreshToken = function (expiresIn = "90 days") {
 const insertRefreshTokenToDB = function (user_uuid) {
 	return new Promise((resolve, reject) => {
 		const refresh_token = generateRefreshToken();
-		const tokenJSON = jwt.verify();
+		const tokenJSON = jwt.verify(refresh_token, process.env.JWT_SECRET);
 
 		const token_value = tokenJSON.token;
 		const issued_at = tokenJSON.iat;
 		const expires_at = tokenJSON.exp;
+		const uuidBinary = uuidToBinary(user_uuid);
 
-		const escaped_values = connection.escape([user_uuid, token_value, issued_at, expires_at, 0]);
+		const escaped_values = connection.escape([uuidBinary, token_value, issued_at, expires_at, 0]);
 		connection.query(
 			`INSERT INTO refresh_token (user_uuid, token, issued_at, expires_at, used) VALUES (${escaped_values})`,
 			function (err) {
