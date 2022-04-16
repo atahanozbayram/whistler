287378c7d0b0e2f49fbc7f4e4141288c476111a7
Atahan <atahan_ozbayram@hotmail.com>
2022-04-16 11:47:40 +0300

    Add tests for dotenv config

---
+++ backend/tests/config/dotenv.test.ts
@@ -0,0 +1,19 @@
+import { config } from "@config/dotenv";
+
+describe("Testing dotenv config file", () => {
+	test("config returns .env.test while testing", () => {
+		expect(config()).toBe(".env.test");
+	});
+
+	test("config returns .env when NODE_ENV is falsy", () => {
+		// assigning process.env.NODE_ENV to undefined doesn't work because it is converted to string 'undefined'
+		// and it evaluates to be truthy. Therefore delete the process.env.NODE_ENV to simulate falsy value.
+		delete process.env.NODE_ENV;
+		expect(config()).toBe(".env");
+	});
+
+	test("config returns .env.prod when NODE_ENV is prod", () => {
+		process.env.NODE_ENV = "prod";
+		expect(config()).toBe(".env.prod");
+	});
+});
