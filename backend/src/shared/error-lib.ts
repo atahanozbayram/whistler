96664adf9df128835b4c2c0d9d588bada6274006
Atahan <atahan_ozbayram@hotmail.com>
2022-05-19 18:07:44 +0300

    Add error message lib to handle error messages
    Add error message lib file to make errors testible by keeping them
    in an object.

---
+++ backend/src/shared/error-lib.ts
@@ -0,0 +1,34 @@
+const ValidationMessages = {
+	exists: function () {
+		return `should exist.`;
+	},
+	notEmpty: function () {
+		return `should be not empty`;
+	},
+	isLength: function ({ min, max }: { min?: number; max?: number }) {
+		if (min && max) {
+			return `should be between ${min}-${max} characters long.`;
+		} else if (min) {
+			return `should be at least ${min} characters long.`;
+		} else if (max) {
+			return `should be maximum ${max} characters long.`;
+		}
+	},
+	isType: function (typeName: string) {
+		return `should be typeof ${typeName}`;
+	},
+};
+
+const ErrorMessages = {
+	controllers: {
+		user: {
+			signUp: {
+				emailInUse: "Email is already in use, enter different one.",
+				usernameInUse: "Username is already in use, enter different one.",
+				emailAndUsernameInUse: "Email and username is already in use, enter different ones.",
+			},
+		},
+	},
+};
+
+export { ValidationMessages, ErrorMessages };
