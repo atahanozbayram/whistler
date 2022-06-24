9756ceabec7aff247c88f3ce5e1870f6623279da
Atahan <atahan_ozbayram@hotmail.com>
2022-06-24 15:53:55 +0300

    Update tests that pass

--- backend/tests/src/shared/user.test.ts
+++ backend/tests/src/shared/user.test.ts
@@ -1,7 +1,16 @@
+import { MockContext, Context, createMockContext } from "@shared/prisma-context";
 import { v1 as uuidv1 } from "uuid";
-import { uuidToBinary, saveUser } from "../../../src/shared/user";
-import { prismaMock } from "@shared/prisma-mock";
+import { uuidToBinary, saveUser, saveVerificationCode } from "@shared/user";
 import { user } from "@prisma/client";
+import { prisma } from "@shared/prisma-original";
+
+let mockCtx: MockContext;
+let ctx: Context;
+
+beforeEach(() => {
+	mockCtx = createMockContext();
+	ctx = mockCtx as unknown as Context;
+});
 
 describe("test shared user functionalities", () => {
 	test("uuidToBinary returns 16 bytes of binary in hex representation", () => {
@@ -21,12 +30,37 @@ describe("test shared user functionalities", () => {
 			birth_date: new Date(1999, 7, 20),
 		};
 
-		prismaMock.user.create.mockResolvedValue(userInfo as unknown as user);
+		mockCtx.prisma.user.create.mockResolvedValue(userInfo as unknown as user);
+
+		saveUser(userInfo, ctx)
+			.then(() => {
+				expect(mockCtx.prisma.user.create.mock.calls.length).toBe(1);
+				done();
+			})
+			.catch((error) => done(error));
+	});
+
+	test("verificationCode is saved when valid user uuid is given", (done) => {
+		const userInfo = {
+			firstname: "Atahan",
+			lastname: "Ozbayram",
+			username: "username1",
+			birth_date: new Date(1999, 7, 20),
+			gender: 2,
+			email: "atahan_ozbayram@hotmail.com",
+			password: "Password1!",
+		};
 
 		saveUser(userInfo)
-			.then(() => {
-				expect(prismaMock.user.create.mock.calls.length).toBe(1);
-				done();
+			.then((user1) => {
+				saveVerificationCode(user1.uuid)
+					.then((code1) => {
+						prisma.verification_code.findFirst({ where: { code: code1, valid: true } }).then((foundCode) => {
+							expect(foundCode?.code).toBe(code1);
+							done();
+						});
+					})
+					.catch((error) => done(error));
 			})
 			.catch((error) => done(error));
 	});
