f9d04e0ae925c83721b31f9ba721359365f07994
Atahan <atahan_ozbayram@hotmail.com>
2022-06-29 18:42:03 +0300

    Add mailer.ts to use nodemailer

---
+++ backend/src/shared/mailer.ts
@@ -0,0 +1,13 @@
+import nodemailer from "nodemailer";
+
+const email_user = process.env.EMAIL_USER;
+const email_password = process.env.EMAIL_PASS;
+
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+	host: "smtp-mail.outlook.com",
+	auth: { user: email_user, pass: email_password },
+	secure: true,
+});
+
+export { transporter };
