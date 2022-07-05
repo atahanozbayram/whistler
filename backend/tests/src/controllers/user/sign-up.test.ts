d789c5639231862431899f152650745a059b5a89
Atahan <atahan_ozbayram@hotmail.com>
2022-07-06 01:13:45 +0300

    Add sign-up related test

--- backend/tests/src/controllers/user/sign-up.test.ts
+++ backend/tests/src/controllers/user/sign-up.test.ts
@@ -2,7 +2,7 @@ import { describe, test } from "@jest/globals";
 import { prisma } from "@shared/prisma-original";
 import request from "supertest";
 import { app } from "@src/app";
-import { signUpRoute } from "@controllers/user/sign-up";
+import { signUpRoute, signUpReqBody } from "@controllers/user/sign-up";
 
 describe("sign-up related tests", () => {
 	app.post("/sign-up", signUpRoute);
@@ -12,6 +12,27 @@ describe("sign-up related tests", () => {
 			.send({})
 			.then((response) => {
 				expect(response.statusCode).toBe(400);
+				done();
+			})
+			.catch((error) => done(error));
+	});
+
+	test("sending fully correct request body returns 200 status code.", (done) => {
+		request(app)
+			.post("/sign-up")
+			.send({
+				firstname: "Atahan",
+				lastname: "Ozbayram",
+				gender: 2,
+				password: "Password1!",
+				password_confirmation: "Password1!",
+				username: "username1",
+				birth_date: "1999-07-20",
+				email: "atahan_ozbayram@hotmail.com",
+			} as signUpReqBody)
+			.then((response) => {
+				console.log("response body: %o", response.body);
+				expect(response.statusCode).toBe(200);
 				done();
 			})
 			.catch((error) => done(error));
