90f84b6630e41e0dd0dae5f610589f95ea830cf0
Atahan <atahan_ozbayram@hotmail.com>
2022-07-06 01:36:46 +0300

    Remove code repetition

--- backend/tests/src/controllers/user/sign-up.test.ts
+++ backend/tests/src/controllers/user/sign-up.test.ts
@@ -6,6 +6,18 @@ import { signUpRoute, signUpReqBody } from "@controllers/user/sign-up";
 
 describe("sign-up related tests", () => {
 	app.post("/sign-up", signUpRoute);
+
+	const signUpReqInstance: signUpReqBody = {
+		firstname: "Atahan",
+		lastname: "Ozbayram",
+		email: "atahan_ozbayram@hotmail.com",
+		gender: 2,
+		password: "Password1!",
+		password_confirmation: "Password1!",
+		username: "username1!",
+		birth_date: "1999-07-20",
+	};
+
 	test("sending fully empty request body returns 400 status code.", (done) => {
 		request(app)
 			.post("/sign-up")
@@ -20,18 +32,8 @@ describe("sign-up related tests", () => {
 	test("sending fully correct request body returns 200 status code.", (done) => {
 		request(app)
 			.post("/sign-up")
-			.send({
-				firstname: "Atahan",
-				lastname: "Ozbayram",
-				gender: 2,
-				password: "Password1!",
-				password_confirmation: "Password1!",
-				username: "username1",
-				birth_date: "1999-07-20",
-				email: "atahan_ozbayram@hotmail.com",
-			} as signUpReqBody)
+			.send(signUpReqInstance)
 			.then((response) => {
-				console.log("response body: %o", response.body);
 				expect(response.statusCode).toBe(200);
 				done();
 			})
