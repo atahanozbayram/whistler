725098a255336e90d9caf620f82c421db6e991e3
Atahan <atahan_ozbayram@hotmail.com>
2022-07-05 18:21:25 +0300

    Add file to seed and clean db for tests

---
+++ backend/tests/seed-and-clean-db.ts
@@ -0,0 +1,12 @@
+import { beforeAll } from "@jest/globals";
+import { prisma } from "@shared/prisma-original";
+import { seed } from "@root/prisma/test-seed";
+
+beforeAll(async () => {
+	if (/test$/i.test(process.env["DATABASE_URL"] as string)) {
+		await prisma.user.deleteMany({ where: {} });
+		await prisma.verification_code.deleteMany({ where: {} });
+		await prisma.gender.deleteMany({});
+		await seed();
+	}
+});
