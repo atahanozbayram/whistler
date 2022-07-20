d401cc6693f98c15dd8c7b2cf0e389c6fc227876
Atahan <atahan_ozbayram@hotmail.com>
2022-07-20 07:06:51 +0300

    Remove requestLogger middleware

--- backend/src/middlewares/request-logger.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { logger } from "@shared/logger";
-
-const requestLogger = function (req: Request, res: Response, next: NextFunction) {
-	logger.info("request: %o", req);
-	next();
-};
-
-export { requestLogger };
