3986277bcb638156ce553c4fb18d29c806dd556c
Atahan <atahan_ozbayram@hotmail.com>
2022-09-09 10:51:59 +0300

    Change saveVerificationCode implementation
    Change the implementation to only save new verification code
    when old one is expired by time or is invalidated.

--- backend/src/shared/user/verification.ts
+++ backend/src/shared/user/verification.ts
@@ -3,34 +3,72 @@ import { transporter } from "@shared/mailer";
 import otpGenerator from "otp-generator";
 import { prisma as prismaOg } from "@shared/prisma-original";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
-import { uuidToBinary } from "@shared/user";
+import date from "date-and-time";
+import { user_verification } from "@prisma/client";
 import { v1 as uuidv1 } from "uuid";
+import { uuidToBinary } from ".";
 
-const saveVerificationCode: (user_uuid: Buffer, ctx?: Context) => Promise<string> = function (
+const saveVerificationCode: (user_uuid: Buffer, ctx?: Context) => Promise<user_verification> = function (
 	user_uuid: Buffer,
 	ctx = { prisma: prismaOg }
 ) {
 	return new Promise((resolve, reject) => {
 		ctx ??= { prisma: prismaOg } as Context;
 		const { prisma } = ctx;
+
+		// first check if the given user is existent then check if the user is not verified.
 		prisma.user
 			.findFirst({ where: { uuid: user_uuid } })
-			.then((userInfo) => {
-				if (userInfo === null) return reject("user not found with given uuid.");
+			.then((user1) => {
+				// if user1 is null, that means it doesn't exist in database
+				if (user1 === null) {
+					return reject("user not found with given uuid.");
+				}
 
-				const randomCode = otpGenerator.generate(6, {
-					digits: true,
-					specialChars: false,
-					lowerCaseAlphabets: false,
-					upperCaseAlphabets: false,
-				});
+				// if user's verified column is true, then we already have verified it, and furter code implementation is unnecessary.
+				if (user1.verified === true) {
+					return reject("user already verified.");
+				}
 
-				prisma.verification_code
-					.create({
-						data: { uuid: uuidToBinary(uuidv1()), code: randomCode, valid: true, user_uuid: user_uuid },
+				// we need to check if there is already an existing verification code which is still valid.
+				// if it exists, resolve immediately with that existing code.
+				prisma.user_verification
+					.findFirst({
+						where: { user_uuid: user1.uuid, valid: true, expires_at: { gt: new Date(Date.now()) } },
 					})
-					.then((verificationEntry) => {
-						resolve(verificationEntry.code);
+					.then((old_valid_code) => {
+						// if there is an old valid code exists, resolve with it.
+						if (old_valid_code !== null) {
+							resolve(old_valid_code);
+							return;
+						}
+
+						// if the old_valid_code doesn't exist, then, create a new one
+						const randomCode = otpGenerator.generate(6, {
+							digits: true,
+							specialChars: false,
+							lowerCaseAlphabets: false,
+							upperCaseAlphabets: false,
+						});
+
+						// save the verification code into the database
+						prisma.user_verification
+							.create({
+								data: {
+									uuid: uuidToBinary(uuidv1()),
+									user_uuid: user1.uuid,
+									code: randomCode,
+									valid: true,
+									attempt_left: 5,
+									created_at: new Date(Date.now()),
+									// give two hours of expiration time.
+									expires_at: date.addHours(new Date(Date.now()), 2),
+								},
+							})
+							.then((user_verification1) => {
+								resolve(user_verification1);
+							})
+							.catch((error) => reject(error));
 					})
 					.catch((error) => reject(error));
 			})
@@ -38,6 +76,7 @@ const saveVerificationCode: (user_uuid: Buffer, ctx?: Context) => Promise<string
 	});
 };
 
+// send the verification code to the user via email.
 const sendVerificationEmail: (
 	{
 		user_email,
