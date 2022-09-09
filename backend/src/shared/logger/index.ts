bf58359d9cad2500db0a7b57d2eade96f7ddfc53
Atahan <atahan_ozbayram@hotmail.com>
2022-09-09 17:27:23 +0300

    Add environment variable DEBUG
    Add DEBUG env variable to make logging to console optional.

--- backend/src/shared/logger/index.ts
+++ backend/src/shared/logger/index.ts
@@ -3,7 +3,7 @@ import { makeDevelopmentLogger, myFormat } from "./development-logger";
 
 const logger = makeDevelopmentLogger();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.DEBUG === "on") {
 	logger.add(
 		new winston.transports.Console({
 			format: winston.format.combine(winston.format.colorize(), winston.format.timestamp(), myFormat),
