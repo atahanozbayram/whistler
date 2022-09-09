bd9adf88f1a35e331c9707153ee07ea3c157efab
Atahan <atahan_ozbayram@hotmail.com>
2022-09-09 10:54:20 +0300

    Update used variable name

--- backend/tests/seed-and-clean-db.ts
+++ backend/tests/seed-and-clean-db.ts
@@ -5,7 +5,7 @@ import { seed } from "@root/prisma/test-seed";
 beforeAll(async () => {
 	if (/test$/i.test(process.env["DATABASE_URL"] as string)) {
 		await prisma.user.deleteMany({ where: {} });
-		await prisma.verification_code.deleteMany({ where: {} });
+		await prisma.user_verification.deleteMany({ where: {} });
 		await prisma.gender.deleteMany({});
 		await seed();
 	}
