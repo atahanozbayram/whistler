0ded85aef5219ee7ee42b237bf15d0f2e43cafb5
Atahan <atahan_ozbayram@hotmail.com>
2022-09-13 18:40:08 +0300

    Add Controller class

---
+++ backend/src/shared/Controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response, NextFunction, Router } from "express";
+import { ValidationChain, validationResult } from "express-validator";
+import _ from "lodash";
+import { requestLogger } from "./request-logger";
+
+abstract class Controller {
+	path: string;
+	router: Router = Router();
+	loggerManip: (req: Request) => Request;
+	validationChain: ValidationChain[];
+	controller: (req: Request, res: Response, next?: NextFunction) => void;
+
+	public setupRouter() {
+		this.router.use(
+			this.path,
+			Controller.loggerMw(),
+			this.validationChain,
+			Controller.validationCheckMw,
+			this.controller
+		);
+	}
+
+	/*
+	 * @param reqManipulator a function manipulates req and resturns it
+	 */
+	public static loggerMw = function (reqManipulator?: (req: Request) => Request) {
+		return function (req: Request, res: Response, next: NextFunction) {
+			let reqCopy = req;
+			if (reqManipulator) {
+				reqCopy = reqManipulator(_.cloneDeep(req));
+			}
+
+			requestLogger(reqCopy);
+			next();
+		};
+	};
+
+	public static validationCheckMw = function (req: Request, res: Response, next: NextFunction) {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		next();
+	};
+}
+
+export { Controller };
