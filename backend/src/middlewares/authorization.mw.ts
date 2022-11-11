38a19b1cc0add3a3678c5b60dc474c76e93e7fc3
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2022-11-11 04:21:52 +0300

    Refactor code to prevent simple error

--- backend/src/middlewares/authorization.mw.ts
+++ backend/src/middlewares/authorization.mw.ts
@@ -2,6 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 const authorizationMw = function(req: Request, res: Response, next: NextFunction) {
+	if (req.headers.authorization === undefined) {
+		res.status(401).json({ message: "unauthorized" });
+		return;
+	}
+
 	const authHeader = req.headers.authorization?.split(" ");
 	if (authHeader[0].toLowerCase() !== "bearer") {
 		res.status(401).json({ message: "unauthorized" });
@@ -9,7 +14,7 @@ const authorizationMw = function(req: Request, res: Response, next: NextFunction
 	}
 
 	const token = authHeader[1];
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err) => {
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err) => {
 		if (err) {
 			res.status(401).json({ message: "unauthorized" });
 			return;
