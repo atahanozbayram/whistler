b713464f36be9058f986e6d7c51faa5233f94767
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2023-01-19 06:48:25 +0300

    Comment file to temporaryly fix issue

--- backend/tests/seed-and-clean-db.ts
+++ backend/tests/seed-and-clean-db.ts
@@ -1,13 +1,13 @@
-import { beforeAll } from "@jest/globals";
-import { prisma } from "@shared/prisma-original";
-import { seed } from "@root/prisma/test-seed";
-
-beforeAll(async () => {
-	if (/test$/i.test(process.env["DATABASE_URL"] as string)) {
-		await prisma.user.deleteMany({ where: {} });
-		await prisma.user_verification.deleteMany({ where: {} });
-		await prisma.gender.deleteMany({});
-		await prisma.refresh_token.deleteMany({});
-		await seed();
-	}
-});
+// import { beforeAll } from "@jest/globals";
+// import { prisma } from "@shared/prisma-original";
+// import { seed } from "@root/prisma/test-seed";
+//
+// beforeAll(async () => {
+// 	if ((process.env["DATABASE_URL"] as string).includes("_test")) {
+// 		await prisma.user.deleteMany({ where: {} });
+// 		await prisma.user_verification.deleteMany({ where: {} });
+// 		await prisma.gender.deleteMany({});
+// 		await prisma.refresh_token.deleteMany({});
+// 		await seed();
+// 	}
+// });
